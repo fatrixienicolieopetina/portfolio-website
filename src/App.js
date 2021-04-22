3f4ecdd2166342cb6896f7e116bea3078e25ee1c
Patricia Nicole Opetina <patricianicole.opetina@gmail.com>
2021-04-23 01:00:41 +0800

    Add navigation header

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
+import logo from './pat1.png';
+import reactlogo from './logo192.png'
 import './App.css';
+import Navigation from './sections/Navigation';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src={reactlogo} className="React-logo" alt="reactlogo" />
+        <Navigation />
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>
+            Hi, I am Pat!
+          </h1>
+        </div>
+
       </header>
     </div>
   );
